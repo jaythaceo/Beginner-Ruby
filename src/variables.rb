9bce5f4c7097c03ecaefe75ee59de18f8d3ee073
jaythaceo <jaythaceo@gmail.com>
2015-01-14 10:58:47 -0500

    [chore] Added comments to variables file

--- src/variables.rb
+++ src/variables.rb
@@ -1,14 +1,15 @@
 
-# Variables
+# String
 myString = "Hello this is a string"
 
 puts myString
 
-# Numbers?
+# Integers and floats
 age = 23
 PI = 3.14
 tax = 0.06
 
+# Puts prints to the screen and object placement
 puts "My age is #{age} amd the state tax is #{tax} what that has to do with anything is beyond me"
 
 puts "This is PI #{PI} "
