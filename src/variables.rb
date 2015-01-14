fe80fa8033f24a6f40a19a108e547059627c6ff9
jaythaceo <jaythaceo@gmail.com>
2015-01-14 02:00:30 -0500

    [Initial] Added src/ dir and variables file

---
+++ src/variables.rb
@@ -0,0 +1,14 @@
+
+# Variables
+myString = "Hello this is a string"
+
+puts myString
+
+# Numbers?
+age = 23
+PI = 3.14
+tax = 0.06
+
+puts "My age is #{age} amd the state tax is #{tax} what that has to do with anything is beyond me"
+
+puts "This is PI #{PI} "
