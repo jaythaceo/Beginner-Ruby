9b351b5c06f17d4788718210ae365e8777db30a2
jaythaceo <jaythaceo@gmail.com>
2015-01-14 20:24:16 -0500

    [chore] Modified arrays and added arrayMethods

---
+++ src/arrayMethods.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+# foods
+foods = ['grapes', 'pizza', 'oranges']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts foods.join(' :) ') + ' 8)'
+
+# push method
+favorites = []
+favorites.push 'StarWars is the best'
+favorites.push 'GI Joe also'
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
--- src/arrays.rb
+++ src/arrays.rb
@@ -8,3 +8,14 @@ puts names[0]
 puts names[1]
 puts names[2] # Always count from zero Jason is 0
 
+puts " " # for a space
+
+languages = ['English', 'German', 'Latin']
+
+languages.each do |lang|
+    puts 'I love ' + lang + '!'
+    puts 'Don\'t you?'
+end
+
+puts 'And let\'s hear it for C++'
+puts '...'
