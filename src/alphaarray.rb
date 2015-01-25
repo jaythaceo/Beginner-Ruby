83bcf1909b6d07f402ae1b24c0228e5c251a0ecf
jaythaceo <jaythaceo@gmail.com>
2015-01-24 19:01:52 -0500

    [chore] Added alpha and multiply

---
+++ src/alphaarray.rb
@@ -0,0 +1,4 @@
+a, b = 0
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+alpha = fruits.sort {|left, right| left <=> right}
+puts alpha
---
+++ src/multiply.rb
@@ -0,0 +1,6 @@
+
+my_array = [1, 2, 3, 4, 5]
+
+my_array.each do |number|
+	puts number * 5
+end
