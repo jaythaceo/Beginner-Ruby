6779ba736dbd83ffb7ca2b879784a8f62fd155fa
jaythaceo <jaythaceo@gmail.com>
2015-02-04 21:11:14 -0500

    [chore] Added more exercises

---
+++ src/hash_Rocket.rb
@@ -0,0 +1,8 @@
+
+#hash
+movies = {
+	action: "Iron Man",
+	anime: "Ghost in The Shell",
+	drama: "Pulp Fiction",
+	classi: "StarWars"
+}
---
+++ src/movies2_10.rb
@@ -0,0 +1,26 @@
+
+movies = {
+	'StarWars' => 5.0
+}
+
+puts "Enter below: "
+user_input = gets.chomp
+
+when "add"
+	puts "Added"
+
+when "update"
+	puts "Updated"
+
+when "display"
+	puts "Display"
+
+when "delete"
+	puts "Deleted"
+
+else
+	puts "Error!"
+
+end
+
+	
