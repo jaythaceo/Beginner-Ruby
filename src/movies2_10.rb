d86d96157a5701c3363c48ccd3ce30bd7e70c322
jaythaceo <jaythaceo@gmail.com>
2015-02-07 22:12:22 -0500

    [chore] Modified movies2_10

--- src/movies2_10.rb
+++ src/movies2_10.rb
@@ -1,39 +1,36 @@
-
 movies = {
-	'StarWars' => 5.0
+ :"Indiana_Jones" => 4,
+ :"Romeo_and_Juliet" => 3.5,
+ :"Finding_Nemo" => 3
 }
 
-puts "Enter below: "
-user_input = gets.chomp
-
-movies = {
-	'StarWars' => 5.0
-}
-
-puts "Enter below: "
-user_input = gets.chomp
+puts "What do you want to do?"
+puts "Type add, update, or delete"
+choice = gets.chomp
 
 case choice
 when "add"
-	puts "Enter a movie title: "
-	title = gets.chomp
-	puts "Enter movie rating: "
-	rating = gets.chomp
-	puts movies[title] = rating
-	puts "The movie and rating was added"
-
+    puts "Movie title?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "Movie rating?"
+        rating = gets.chomp
+        movies[title.to_sym] = rating.to_i
+        puts "Movie added!"
+    else puts "Movie already exists"
+    end
 when "update"
-	puts "Updated"
-
-when "display"
-	puts "Display"
-
-when "delete"
-	puts "Deleted"
-
-else
-	puts "Error!"
-
+    puts "Update which movie?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "This movie is not included"
+else puts "New rating?"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been updated with rating of #{rating}"
 end
-
-	
+when "delete"
+    puts "Deleted!"
+else
+    puts "Error!"
+end
