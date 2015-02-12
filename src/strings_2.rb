a2d04a46a5e4bafe823353d5e6b456b88dcd5c93
jaythaceo <jaythaceo@gmail.com>
2015-02-12 13:30:55 -0500

    [chore] Fixed string hash

--- src/strings_2.rb
+++ src/strings_2.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 
+# Iterate over the data structure and build a string
 hash = {key1: "val2", key2: "val1" }
 string = ""
 hash.each { |k,v| string << "#{k} is #{v}\n" }
 puts string
+
+# If data structure is an array or easily tramsformed into array use
+# Array#join
+puts hash.keys.join("\n") + "\n"
