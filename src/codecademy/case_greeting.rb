003a7c8fcf9e61e2e92fabfa12f7fd32f9c5901d
jaythaceo <jaythaceo@gmail.com>
2015-02-12 20:23:16 -0500

    [modified] case_greeting

--- src/codecademy/case_greeting.rb
+++ src/codecademy/case_greeting.rb
@@ -4,3 +4,15 @@ puts "Hello there"
 greeting = gets.chomp
 
 # Add your case statement below
+case greeting
+when "English"
+	puts "Hello"
+when "French"
+	puts "Bonjour"
+when "German"
+	puts "Guten"
+when "Finnish"
+	puts "Haloo"
+else
+	puts "I don't know that language"
+end
